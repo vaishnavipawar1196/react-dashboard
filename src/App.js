21d1a8e1204fac16bad874184ac49acec458ce99
Vaishnavi Pawar <vaishnavipawar1196@gmail.com>
2024-06-21 09:55:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import './App.css';
 import Dashboard from './pages/dashboard/Dashboard';
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,Navigate,RouterProvider } from "react-router-dom";
 import Transactions from './pages/transaction/Transactions';
 import Support from './pages/support/Support';
-import ErrorBoundry from './components/ErrorBoundry';
-import ErrorPage from './components/ErrorPage';
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Dashboard />,
-      errorElement: <ErrorPage />, // Error page for the root path
     },
     {
       path: "/transactions",
       element: <Transactions />,
-      errorElement: <ErrorPage />, // Error page for the root path
     },
     {
       path: "/support",
       element: <Support />,
-      errorElement: <ErrorPage />, // Error page for the root path
+    },
+    {
+      path: '*',
+      element: <Navigate to="/" />
     },
   ]);
 
 function App() {
   return (
     <>
-    <ErrorBoundry>
       <RouterProvider router={router} />
-    </ErrorBoundry>
     </>
   );
 }
